f9af7b29808974229fc560fa1263b981da66a37b
10dimensions <relay.10d@gmail.com>
2021-10-01 14:19:39 +0530

    Tokenlist Update Check 01/10/2021

--- pub.sh
+++ pub.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/bash
+current=$(jq '.tokens | length' ./euler-tokenlist.json)
+echo "Current Tokens $current"
 node ./index.js
+
+latest=$(jq '.tokens | length' ./euler-tokenlist.json)
+echo "Latest Tokens $latest"
+
+if [[ $latest == $current ]]
+then
+    dt=$(date '+%d/%m/%Y');
+    echo "$dt"
+
+    git pull
+    git add .
+    git commit -m "Tokenlist Update Check $dt"
+    git push
+fi
