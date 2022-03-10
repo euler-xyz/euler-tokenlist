bb72d59d9cd651720a631649cd78a8ed50c50f47
darek <dariushg32111@gmail.com>
2022-03-10 11:43:42 +0100

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "update-euler-tokenlist",
+      script: "./scripts/updateEulerList.js",
+      watch: false,
+      autorestart: false,
+      cron_restart: "0 22 * * * *", // every day at 10pm
+    },
+  ],
+};
