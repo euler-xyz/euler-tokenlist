eea95416558973a224febf803d866990c734c594
darek <dariushg32111@gmail.com>
2022-08-09 09:50:14 +0200

    update removed list

--- curated/remove.js
+++ curated/remove.js
@@ -8,4 +8,22 @@ module.exports = [
     "decimals": 18,
     "logoURI": "https://assets.coingecko.com/coins/images/17206/thumb/wild.PNG?1626848440"
   },
+  {
+    "chainId": 1,
+    "address": "0x321c2fe4446c7c963dc41dd58879af648838f98d",
+    "name": "Cryptex Finance",
+    "symbol": "CTX",
+    "decimals": 18,
+    "logoURI": "https://assets.coingecko.com/coins/images/14932/thumb/glossy_icon_-_C200px.png?1619073171",
+    "extensions": {
+      "permit": {
+        "type": "EIP2612",
+        "domain": {
+          "name": "Cryptex",
+          "chainId": 1,
+          "verifyingContract": "0x321c2fe4446c7c963dc41dd58879af648838f98d"
+        }
+      }
+    }
+  },
 ];
