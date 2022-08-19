27e0205836e9b8e21e9fa2c300ad4ddc1975971e
darek <dariushg32111@gmail.com>
2022-08-19 15:27:57 +0200

    add brew shellenv

--- scripts/git/commit.sh
+++ scripts/git/commit.sh
@@ -7,4 +7,7 @@ echo "$dt"
 git add .
 git commit -m "Tokenlist Update $dt"
 git push -u origin $branch
+
+# TODO: fix brew on ec2 not to require next line 
+eval $(~/.linuxbrew/bin/brew shellenv)
 hub pull-request -m "Tokenlist Update $dt"
